6270faa199ba4847af56f03336293a4cd77485c8
david116 <david116_lee@163.com>
2016-06-21 13:24:17 +0800

    Update mysql.php

--- public/lib/mysql.php
+++ public/lib/mysql.php
@@ -3,7 +3,7 @@
 function connect_db() {
     $server = 'localhost'; // this may be an ip address instead
     $user = 'root';
-    $pass = 'ds332527';
+    $pass = 'ds332527';    // change to your db password
     $database = 'rest'; // name of your database
     $connection = new mysqli($server, $user, $pass, $database);
  
